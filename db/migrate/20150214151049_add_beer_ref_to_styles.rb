cd682084337ec68926c512c63c15aa956d1b07e4
Lionking93 <leo.kallonen@gmail.com>
2015-02-14 19:16:32 +0200

    viidennen viikon lisays

--- db/migrate/20150214151049_add_beer_ref_to_styles.rb
+++
@@ -1,6 +0,0 @@
-class AddBeerRefToStyles < ActiveRecord::Migration
-  def change
-    add_reference :styles, :beer, index: true
-    add_foreign_key :styles, :beers
-  end
-end
--- db/migrate/20150214153402_remove_beer_ref_from_styles.rb
+++
@@ -1,6 +0,0 @@
-class RemoveBeerRefFromStyles < ActiveRecord::Migration
-  def change
-    remove_reference :styles, :beer, index: true
-    remove_foreign_key :styles, :beers
-  end
-end
